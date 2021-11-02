85ec9159cf35045027dddb7af4b90bb6be2c5b42
Levi Huillet <levimhuillet@gmail.com>
2021-11-01 21:13:40 -0500

    completed milestone 1

--- app/src/main/java/com/example/lab6_milestone1_huillet/MainActivity.java
+++ app/src/main/java/com/example/lab6_milestone1_huillet/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Somewhere in Australia
+    private final LatLng mDestinationLatLng = new LatLng(43.0757378,-89.4061951);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            // code to display marker
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
